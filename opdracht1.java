ac88fa305240b659e38cdf10fbcf3df987c593dc
annie410 <angharad.macleod@gmail.com>
2021-02-09 10:36:52 +0100

    first commit

---
+++ opdracht1.java
@@ -0,0 +1,12 @@
+
+class opdracht1 {
+	public static void main (String[] args) {
+		
+		String po = new String("po");
+		String ging = new String("ging");
+		String en = new String("en");
+		
+		System.out.println(po+ging+en);
+	}
+
+}
